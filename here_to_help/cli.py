5749fb2cee707275902a23d39e76902faf7bcde9
Dmitry Manannikov <email@slonoed.net>
2023-09-02 21:18:49 -0700

    Model selection

--- here_to_help/cli.py
+++ here_to_help/cli.py
@@ -4,7 +4,15 @@ import os
 from iterfzf import iterfzf
 from here_to_help.prompts_parser import parse_text
 
-gpt4 = guidance.llms.OpenAI("gpt-4")
+default_model = 'gpt-4'
+
+def get_model(name):
+    match name:
+        case "gpt-3.5-turbo":
+            return guidance.llms.OpenAI("gpt-3.5-turbo")
+        case "gpt-4":
+            return guidance.llms.OpenAI("gpt-4")
+    exit(f"Model not found: {name}")
 
 def main():
     parser = argparse.ArgumentParser(description='Here To Help CLI tool.')
@@ -22,41 +30,39 @@ def main():
 
     with open(args.prompts_file, 'r') as f:
         content = f.read()
+        parsed_prompts = parse_text(content)
+        titles = [prompt['title'] for prompt in parsed_prompts]
+        selected_title = iterfzf(titles)
 
-    parsed_prompts = parse_text(content)
-    titles = [prompt['title'] for prompt in parsed_prompts]
-    selected_title = iterfzf(titles)
+        if not selected_title:
+            exit(f"No prompt selected")
 
-    if not selected_title:
-        exit(f"No prompt selected")
+        for prompt in parsed_prompts:
+            if prompt['title'] == selected_title:
+                name_values = {}
+                for name in prompt['inputs']:
+                    user_value = input(f"Please enter a value for {name}: ")
+                    name_values[name] = user_value
+                llm = get_model(prompt.get('model', default_model))
+                program = guidance(prompt['content'], llm=llm)
+                out = ""
+                def r(s):
+                    nonlocal out
+                    out = out + "\n\n" + s
 
-    prompt = next((p for p in parsed_prompts if p['title'] == selected_title), None)
-    if prompt is None:
-        exit("Selected prompt not found.")
+                name_values['out'] = r
 
-    name_values = {name: input(f"Please enter a value for {name}: ") for name in prompt['inputs']}
+                program_args = {key: value for key, value in name_values.items()}
+                result = program(**program_args)
 
-    program = guidance(prompt['content'], llm=gpt4) # THIS SHOULD PASTE PROPER CONTENT
+                if out != "":
+                    print(out)
+                elif result.variables().get('output') != None:
+                    print(result.variables()['output'])
+                else:
+                    print(result)
 
-    out = ""
-    def r(s):
-        nonlocal out
-        out = out + "\n\n" + s
-
-    name_values['out'] = r
-
-    program_args = {key: value for key, value in name_values.items()}
-    result = program(**program_args)
-
-    if out:
-        print(out)
-        return
-
-    if result.variables().get('output') is not None:
-        print(result.variables()['output'])
-        return
-
-    print(result)
+                break
 
 if __name__ == '__main__':
     main()
