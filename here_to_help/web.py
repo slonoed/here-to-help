b48ea875d0f0b14b102d8cb6fbcd9efb6cf22050
Dmitry Manannikov <email@slonoed.net>
2023-09-04 22:05:25 -0700

    HTMX works

--- here_to_help/web.py
+++ here_to_help/web.py
@@ -2,12 +2,15 @@ import os
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.responses import PlainTextResponse
+from starlette.responses import Response
 from starlette.routing import Route
 from starlette.staticfiles import StaticFiles
 from starlette.routing import Mount
 from asgi_htmx import HtmxMiddleware
 from starlette.middleware import Middleware
 from starlette.templating import Jinja2Templates
+from asgi_htmx import HtmxMiddleware
+from asgi_htmx import HtmxRequest as Request
 
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -19,8 +22,12 @@ templates = Jinja2Templates(directory=templates_directory)
 async def homepage(request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+async def result(request: Request) -> Response:
+    return PlainTextResponse("Hello, world!")
+
 app = Starlette(debug=True, routes=[
     Mount('/static', app=StaticFiles(directory=static_directory), name="static"),
+    Route("/result", result, name="result"),
     Route('/', homepage),
 ], middleware=[
     Middleware(HtmxMiddleware),
